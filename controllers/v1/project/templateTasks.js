7d29e4f90cd92acd4ef6c570d69e28cd91584728
prajwal <prajwal@tunerlabs.com>
2024-05-12 22:57:23 +0530

    userId fetch correction in new API

--- controllers/v1/project/templateTasks.js
+++ controllers/v1/project/templateTasks.js
@@ -6,138 +6,124 @@
  */
 
 // Dependencies
-const csv = require('csvtojson');
-const projectTemplateTasksHelper = require(MODULES_BASE_PATH + "/project/templateTasks/helper");
+const csv = require('csvtojson')
+const projectTemplateTasksHelper = require(MODULES_BASE_PATH + '/project/templateTasks/helper')
 
- /**
-    * ProjectTemplateTasks
-    * @class
-*/
+/**
+ * ProjectTemplateTasks
+ * @class
+ */
 
 module.exports = class ProjectTemplateTasks extends Abstract {
+	/**
+	 * @apiDefine errorBody
+	 * @apiError {String} status 4XX,5XX
+	 * @apiError {String} message Error
+	 */
 
-    /**
-     * @apiDefine errorBody
-     * @apiError {String} status 4XX,5XX
-     * @apiError {String} message Error
-     */
+	/**
+	 * @apiDefine successBody
+	 *  @apiSuccess {String} status 200
+	 * @apiSuccess {String} result Data
+	 */
 
-    /**
-     * @apiDefine successBody
-     *  @apiSuccess {String} status 200
-     * @apiSuccess {String} result Data
-     */
-    
-    constructor() {
-        super("project-template-tasks");
-    }
+	constructor() {
+		super('project-template-tasks')
+	}
 
-    /**
-    * @api {post} /project/v1/project/templateTasks/bulkCreate/:projectTemplateId 
-    * Bulk create project template tasks.
-    * @apiVersion 1.0.0
-    * @apiGroup Project Template Tasks
-    * @apiParam {File} projectTemplateTasks Mandatory project template tasks file of type CSV.
-    * @apiSampleRequest /project/v1/project/templateTasks/bulkCreate/5f2adc57eb351a5a9c68f403
-    * @apiUse successBody
-    * @apiUse errorBody
-    */
+	/**
+	 * @api {post} /project/v1/project/templateTasks/bulkCreate/:projectTemplateId
+	 * Bulk create project template tasks.
+	 * @apiVersion 1.0.0
+	 * @apiGroup Project Template Tasks
+	 * @apiParam {File} projectTemplateTasks Mandatory project template tasks file of type CSV.
+	 * @apiSampleRequest /project/v1/project/templateTasks/bulkCreate/5f2adc57eb351a5a9c68f403
+	 * @apiUse successBody
+	 * @apiUse errorBody
+	 */
 
-      /**
-      * Upload project template tasks
-      * @method
-      * @name bulkCreate
-      * @returns {JSON} returns uploaded project templates.
-     */
+	/**
+	 * Upload project template tasks
+	 * @method
+	 * @name bulkCreate
+	 * @returns {JSON} returns uploaded project templates.
+	 */
 
-    async bulkCreate(req) {
-        return new Promise(async (resolve, reject) => {
-            try {
-                if ( !req.files || !req.files.projectTemplateTasks ) {
-                    return resolve(
-                      { 
-                        message : CONSTANTS.apiResponses.PROJECT_TEMPLATES_TASKS_CSV
-                      }
-                    )
-                }
+	async bulkCreate(req) {
+		return new Promise(async (resolve, reject) => {
+			try {
+				if (!req.files || !req.files.projectTemplateTasks) {
+					return resolve({
+						message: CONSTANTS.apiResponses.PROJECT_TEMPLATES_TASKS_CSV,
+					})
+				}
 
-                const templateTasks = 
-                await csv().fromString(req.files.projectTemplateTasks.data.toString());
+				const templateTasks = await csv().fromString(req.files.projectTemplateTasks.data.toString())
 
-                const projectTemplateTasks = 
-                await projectTemplateTasksHelper.bulkCreate(
-                    templateTasks,
-                    req.params._id,
-                    req.userDetails.userInformation.userId
-                );
+				const projectTemplateTasks = await projectTemplateTasksHelper.bulkCreate(
+					templateTasks,
+					req.params._id,
+					req.userDetails.userInformation.userId
+				)
 
-                return resolve(projectTemplateTasks);
+				return resolve(projectTemplateTasks)
+			} catch (error) {
+				return reject({
+					status: error.status || HTTP_STATUS_CODE.internal_server_error.status,
+					message: error.message || HTTP_STATUS_CODE.internal_server_error.message,
+					errorObject: error,
+				})
+			}
+		})
+	}
 
-            } catch (error) {
-                return reject({
-                    status: error.status || HTTP_STATUS_CODE.internal_server_error.status,
-                    message: error.message || HTTP_STATUS_CODE.internal_server_error.message,
-                    errorObject: error
-                });
-            }
-        })
-    }
+	/**
+	 * @api {post} /project/v1/project/templateTasks/bulkUpdate/:projectTemplateId
+	 * Bulk update project template tasks.
+	 * @apiVersion 1.0.0
+	 * @apiGroup Project Template Tasks
+	 * @apiParam {File} projectTemplateTasks Mandatory project template tasks file of type CSV.
+	 * @apiSampleRequest /project/v1/project/templateTasks/bulkUpdate/5f2adc57eb351a5a9c68f403
+	 * @apiUse successBody
+	 * @apiUse errorBody
+	 */
 
-     /**
-    * @api {post} /project/v1/project/templateTasks/bulkUpdate/:projectTemplateId 
-    * Bulk update project template tasks.
-    * @apiVersion 1.0.0
-    * @apiGroup Project Template Tasks
-    * @apiParam {File} projectTemplateTasks Mandatory project template tasks file of type CSV.
-    * @apiSampleRequest /project/v1/project/templateTasks/bulkUpdate/5f2adc57eb351a5a9c68f403
-    * @apiUse successBody
-    * @apiUse errorBody
-    */
+	/**
+	 * Upload project template tasks
+	 * @method
+	 * @name bulkUpdate
+	 * @returns {JSON} returns uploaded project templates.
+	 */
 
-      /**
-      * Upload project template tasks
-      * @method
-      * @name bulkUpdate
-      * @returns {JSON} returns uploaded project templates.
-     */
+	async bulkUpdate(req) {
+		return new Promise(async (resolve, reject) => {
+			try {
+				if (!req.files || !req.files.projectTemplateTasks) {
+					return resolve({
+						message: CONSTANTS.apiResponses.PROJECT_TEMPLATES_TASKS_CSV,
+					})
+				}
 
-    async bulkUpdate(req) {
-        return new Promise(async (resolve, reject) => {
-            try {
-                
-                if ( !req.files || !req.files.projectTemplateTasks ) {
-                    return resolve(
-                      {
-                        message : CONSTANTS.apiResponses.PROJECT_TEMPLATES_TASKS_CSV
-                      }
-                    )
-                }
+				const templateTasks = await csv().fromString(req.files.projectTemplateTasks.data.toString())
 
-                const templateTasks = 
-                await csv().fromString(
-                    req.files.projectTemplateTasks.data.toString()
-                );
+				const projectTemplateTasks = await projectTemplateTasksHelper.bulkUpdate(
+					templateTasks,
+					req.params._id,
+					req.userDetails.userInformation.userId
+				)
 
-                const projectTemplateTasks = 
-                await projectTemplateTasksHelper.bulkUpdate(
-                    templateTasks,
-                    req.params._id,
-                    req.userDetails.userInformation.userId
-                );
+				return resolve(projectTemplateTasks)
+			} catch (error) {
+				return reject({
+					status: error.status || HTTP_STATUS_CODE.internal_server_error.status,
+					message: error.message || HTTP_STATUS_CODE.internal_server_error.message,
+					errorObject: error,
+				})
+			}
+		})
+	}
 
-                return resolve(projectTemplateTasks);
-
-            } catch (error) {
-                return reject({
-                    status: error.status || HTTP_STATUS_CODE.internal_server_error.status,
-                    message: error.message || HTTP_STATUS_CODE.internal_server_error.message,
-                    errorObject: error
-                });
-            }
-        })
-    }
-
-    /**
+	/**
     * @api {post} /project/v1/project/templateTasks/update/:taskId 
     * Update projects template.
     * @apiVersion 1.0.0
@@ -154,35 +140,32 @@ module.exports = class ProjectTemplateTasks extends Abstract {
       }
     */
 
-      /**
-      * Update project templates task
-      * @method
-      * @name update
-      * @returns {JSON} returns uploaded project template task.
-     */
+	/**
+	 * Update project templates task
+	 * @method
+	 * @name update
+	 * @returns {JSON} returns uploaded project template task.
+	 */
 
-    async update(req) {
-        return new Promise(async (resolve, reject) => {
-            try {
+	async update(req) {
+		return new Promise(async (resolve, reject) => {
+			try {
+				let projectTemplateTask = await projectTemplateTasksHelper.update(
+					req.params._id,
+					req.body,
+					req.userDetails.userInformation.userId
+				)
 
-                let projectTemplateTask = await projectTemplateTasksHelper.update(
-                  req.params._id, 
-                  req.body, 
-                  req.userDetails.userInformation.userId
-                );
+				projectTemplateTask.result = projectTemplateTask.data
 
-                projectTemplateTask.result = projectTemplateTask.data;
-
-                return resolve(projectTemplateTask);
-
-            } catch (error) {
-                return reject({
-                    status: error.status || HTTP_STATUS_CODE.internal_server_error.status,
-                    message: error.message || HTTP_STATUS_CODE.internal_server_error.message,
-                    errorObject: error
-                });
-            }
-        })
-    }
-
-};
+				return resolve(projectTemplateTask)
+			} catch (error) {
+				return reject({
+					status: error.status || HTTP_STATUS_CODE.internal_server_error.status,
+					message: error.message || HTTP_STATUS_CODE.internal_server_error.message,
+					errorObject: error,
+				})
+			}
+		})
+	}
+}
