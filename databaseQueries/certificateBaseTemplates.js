030ac732c678f26bbfdee97f8ced05c089ab960b
prajwal <prajwal@tunerlabs.com>
2024-06-05 08:34:46 +0530

    certificate-implementation without sunbird rd

---
+++ databaseQueries/certificateBaseTemplates.js
@@ -0,0 +1,72 @@
+/**
+ * name : certificateBaseTemplates.js
+ * author : prajwal
+ * created-date : 21-May-2024
+ * Description : Certificate base template helper for DB interactions.
+ */
+
+// Dependencies
+
+/**
+ * CertificateBaseTemplates
+ * @class
+ */
+
+module.exports = class CertificateBaseTemplates {
+	/**
+	 * certificate base template create.
+	 * @method
+	 * @name create
+	 * @param {JSON} data - certificate base template bodyData.
+	 * @returns {JSON} certificateBaseTemplate creation details.
+	 */
+
+	static create(data) {
+		return new Promise(async (resolve, reject) => {
+			try {
+				let certificateBaseTemplateDoc = await database.models.certificateBaseTemplates.create(data)
+
+				return resolve(certificateBaseTemplateDoc)
+			} catch (error) {
+				return reject(error)
+			}
+		})
+	}
+
+	/**
+	 * certificate base template update.
+	 * @method
+	 * @name update
+	 * @param {JSON} filterData - certificate base template filter query.
+	 * @param {JSON} setData - set data.
+	 * @param {Boolean} retrunData - true/false to return updated data
+	 * @returns {JSON} certificateBaseTemplates updated details.
+	 */
+
+	static update(filterData, setData, retrunData = { new: true }) {
+		return new Promise(async (resolve, reject) => {
+			try {
+				let certificateBaseTemplateDoc = await database.models.certificateBaseTemplates
+					.findOneAndUpdate(filterData, setData, retrunData)
+					.lean()
+
+				return resolve(certificateBaseTemplateDoc)
+			} catch (error) {
+				return reject(error)
+			}
+		})
+	}
+
+	static findDocument(filterData, fieldsArray) {
+		return new Promise(async (resolve, reject) => {
+			try {
+				let baseTemplateData = await database.models.certificateBaseTemplates
+					.find(filterData, fieldsArray)
+					.lean()
+				return resolve(baseTemplateData)
+			} catch (error) {
+				return reject(error)
+			}
+		})
+	}
+}
--- databaseQueries/certificateTemplates.js
+++ databaseQueries/certificateTemplates.js
@@ -5,56 +5,75 @@
  * Description : Certificate template helper for DB interactions.
  */
 
-// Dependencies 
+// Dependencies
 
 /**
-    * CertificateTemplates
-    * @class
-*/
+ * CertificateTemplates
+ * @class
+ */
 
-module.exports= class CertificateTemplates{
-    /**
-     * certificate template details.
-     * @method
-     * @name certificateTemplateDocument
-     * @param {Array} [filterData = "all"] - certificate template filter query.
-     * @param {Array} [fieldsArray = "all"] - projected fields.
-     * @param {Array} [skipFields = "none"] - field not to include
-     * @returns {Array} certificateTemplates details.
-    */
-    
-    static certificateTemplateDocument(
-        filterData = "all", 
-        fieldsArray = "all",
-        skipFields = "none"
-    ) {
-        return new Promise(async (resolve, reject) => {
-            try {
-                let queryObject = (filterData != "all") ? filterData : {};
-                let projection = {}
-           
-                if (fieldsArray != "all") {
-                    fieldsArray.forEach(field => {
-                        projection[field] = 1;
-                   });
-               }
-               
-               if( skipFields !== "none" ) {
-                   skipFields.forEach(field=>{
-                       projection[field] = 0;
-                   });
-               }
-               let certificateTemplateDoc = 
-               await database.models.certificateTemplates.find(
-                   queryObject, 
-                   projection
-               ).lean();
-           
-               return resolve(certificateTemplateDoc);
-           
-            } catch (error) {
-                return reject(error);
-            }
-        });
-    }
+module.exports = class CertificateTemplates {
+	/**
+	 * certificate template details.
+	 * @method
+	 * @name certificateTemplateDocument
+	 * @param {Array} [filterData = "all"] - certificate template filter query.
+	 * @param {Array} [fieldsArray = "all"] - projected fields.
+	 * @param {Array} [skipFields = "none"] - field not to include
+	 * @returns {Array} certificateTemplates details.
+	 */
+
+	static certificateTemplateDocument(filterData = 'all', fieldsArray = 'all', skipFields = 'none') {
+		return new Promise(async (resolve, reject) => {
+			try {
+				let queryObject = filterData != 'all' ? filterData : {}
+				let projection = {}
+
+				if (fieldsArray != 'all') {
+					fieldsArray.forEach((field) => {
+						projection[field] = 1
+					})
+				}
+
+				if (skipFields !== 'none') {
+					skipFields.forEach((field) => {
+						projection[field] = 0
+					})
+				}
+				let certificateTemplateDoc = await database.models.certificateTemplates
+					.find(queryObject, projection)
+					.lean()
+
+				return resolve(certificateTemplateDoc)
+			} catch (error) {
+				return reject(error)
+			}
+		})
+	}
+
+	static createCertificateTemplate(data) {
+		return new Promise(async (resolve, reject) => {
+			try {
+				let certificateTemplateDoc = await database.models.certificateTemplates.create(data)
+
+				return resolve(certificateTemplateDoc)
+			} catch (error) {
+				return reject(error)
+			}
+		})
+	}
+
+	static updateCertificateTemplate(filterData, setData, retrunData = { new: true }) {
+		return new Promise(async (resolve, reject) => {
+			try {
+				let certificateBaseTemplateDoc = await database.models.certificateTemplates
+					.findOneAndUpdate(filterData, setData, retrunData)
+					.lean()
+
+				return resolve(certificateBaseTemplateDoc)
+			} catch (error) {
+				return reject(error)
+			}
+		})
+	}
 }
